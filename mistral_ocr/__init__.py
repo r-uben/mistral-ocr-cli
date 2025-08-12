661c95e63985e6a749d491854de38ca0bf4c6b42
Ruben Fernández-Fuertes <fernandezfuertesruben@gmail.com>
2025-08-12 23:44:13 +0200

    Update author name to Fernandez-Fuertes and add legal disclaimers

--- mistral_ocr/__init__.py
+++ mistral_ocr/__init__.py
@@ -1,7 +1,7 @@
 """Mistral OCR CLI - A clean command-line tool for OCR processing using Mistral AI."""
 
 __version__ = "1.0.0"
-__author__ = "Ruben Fernandez"
+__author__ = "Ruben Fernandez-Fuertes"
 
 from .processor import OCRProcessor
 from .config import Config
